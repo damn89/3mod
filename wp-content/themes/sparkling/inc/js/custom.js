d0f9d167968ac5ff49c1462d6f52a65436f14604
schmolie <tinahakansson@gmail.com>
2016-03-29 13:49:42 +0200

    no message

--- wp-content/themes/sparkling/inc/js/custom.js
+++ wp-content/themes/sparkling/inc/js/custom.js
@@ -1,19 +1,29 @@
 (function($){ $(function(){
 
+var url = 'wp-content/uploads/byggnader';
+
 // Get Grandparent
 for (var j = 0; j < dirData.length; j++) {
-	//console.log('Nuuuu då? ', dirData[j]);
-	console.log('Grandparent: ', dirData[j].path);
-	
-	// Get Parent object
+
+	var html = $('<li class="dropdown"/>');
+	html.append('<a href="'+ dirData[j].path +'">'+ dirData[j].filename +'</a>');
+	$('.navbar-nav').append(html); 
+
+	// Get Parent
+	var subMenu = '';
 	dirData[j].children.forEach(function(parent) {
 	  console.log('Parent: ', parent.path);
 
+		subMenu += ('<li><a href="'+ url + parent.path +'">'+ parent.filename +'</a></li>');
+
+		// Get Children
 		parent.children.forEach(function(children) {
 				console.log('Children: ', children.path);
 		});
 
-	});
+	}); html.append('<ul role="menu" class="dropdown-menu">'+ subMenu +'</ul>');
 }
 
 }); })(jQuery);
+
+
